207995dec66e3bd35fed7ea8360feff3fe2a8f8b
Zedrick John Villas <zedrickvillas@Zedricks-MacBook-Pro.local>
2018-01-29 13:08:23 +0800

    Add Docker Files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pondo
                 </div>
 
                 <div class="links">
