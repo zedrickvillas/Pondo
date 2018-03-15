8c228f53b96adb398591d7fe26364db342b5b756
Joshua O. Morales <joshuao.moralesjm@gmail.com>
2018-03-15 23:28:33 +0800

    Add package Domainos for filtering out temporary emails

---
+++ config/domainos.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'block' => [
+        // Domains you want to block e.g mailinator.com
+    ],
+
+
+    'allow' => [
+        // Domains you want to allow e.g gmail.com
+        'gmail.com',
+        'yahoo.com',
+        'yahoo.com.ph',
+        'hotmail.com',
+        'live.com',
+    ],
+];
